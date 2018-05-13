a86ce0e1be4e98113b21d0560095dca518cf7840
Raghu Betina <raghu@firstdraft.com>
2018-05-13 05:14:41 +0000

    Generate lenders resource with starter:resource

---
+++ db/migrate/20180513051441_create_lenders.rb
@@ -0,0 +1,12 @@
+class CreateLenders < ActiveRecord::Migration
+  def change
+    create_table :lenders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+
+      t.timestamps
+
+    end
+  end
+end
